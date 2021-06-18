d78b85dc328f80e9585ad62cbd7a6bc422693b6a
Bartolomeu <pbartmr@gmail.com>
2021-06-18 22:34:05 +0100

    bundle-declaration-references script improvements

--- packages/not-me/scripts/bundle-declaration-references.js
+++ packages/not-me/scripts/bundle-declaration-references.js
@@ -1,17 +1,18 @@
 const glob = require("glob");
-const { promisify } = require("util");
 const fs = require("fs");
-const writeFile = promisify(fs.writeFile);
 
 const declarationFiles = glob.sync("lib/**/*.d.ts");
 
-const file = declarationFiles.reduce((content, current) => {
+const typesFile = "lib/types.d.ts";
+
+const declarationReferences = declarationFiles.reduce((content, current) => {
+  if (current === typesFile) {
+    return content;
+  }
+
   const relativePath = current.substring(4);
 
   return content + `/// <reference path="./${relativePath}" />` + "\n";
 }, "");
 
-writeFile("lib/index.d.ts", file).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+fs.writeFileSync(typesFile, declarationReferences);
