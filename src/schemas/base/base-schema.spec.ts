c9eb2e615522bc66fe15149abb9c5eaae8c42419
Bartolomeu <pbartmr@gmail.com>
2022-02-02 22:47:22 +0000

    Test improvements

--- src/schemas/base/base-schema.spec.ts
+++ src/schemas/base/base-schema.spec.ts
@@ -10,7 +10,7 @@ describe("Base Schema", () => {
         .test((v) => (v < 1000 ? null : "Must be smaller than 1000")),
     }).required();
 
-    expect(schema.validate({ a: 1000.1 }, { abortEarly: false })).toEqual({
+    expect(schema.validate({ a: 1000.1 })).toEqual({
       errors: true,
       messagesTree: [
         {
@@ -20,7 +20,7 @@ describe("Base Schema", () => {
     });
   });
 
-  it("Fail with single error message on abort early", () => {
+  it("Fail at the first failed condition when abortEarly is true", () => {
     const schema = object({
       a: number()
         .integer()
@@ -38,24 +38,24 @@ describe("Base Schema", () => {
     });
   });
 
-  it("Send transformed value to chained validations that follow", () => {
+  it("Send transformed value to validations that follow, even when abortEarly is true", () => {
     const schema = number()
       .integer()
-      .transform(() => true)
-      .test((v) => (v === true ? null : "Must be true"));
+      .transform(() => "transformed-value")
+      .test((v) => (v === "transformed-value" ? null : "invalid"));
 
     expect(schema.validate(1, { abortEarly: true })).toEqual({
       errors: false,
-      value: true,
+      value: "transformed-value",
     });
   });
 
-  it("Schemas that call required() should stop the value from continuing the validation chain", () => {
+  it("Stop validating a value if it didn't pass the required() condition", () => {
     const lastTransform = jest.fn();
 
     const schema = number().integer().required().test(lastTransform);
 
-    schema.validate(undefined, { abortEarly: false });
+    schema.validate(undefined);
 
     expect(lastTransform).not.toHaveBeenCalled();
   });
@@ -63,18 +63,44 @@ describe("Base Schema", () => {
   it("require() should catch both null and undefined", () => {
     const schema = number().integer().required();
 
-    const undefinedRes = schema.validate(undefined, { abortEarly: false });
+    const undefinedRes = schema.validate(undefined);
 
     expect(undefinedRes).toEqual({
       errors: true,
       messagesTree: ["Input is required"],
     });
 
-    const nullRes = schema.validate(null, { abortEarly: false });
+    const nullRes = schema.validate(null);
 
     expect(nullRes).toEqual({
       errors: true,
       messagesTree: ["Input is required"],
+    });
+  });
+
+  it("Should still consider a value as invalid if the first condition fails but the last one passes", () => {
+    const schema = object({})
+      .test(() => "Failed")
+      .test(() => null);
+
+    const res = schema.validate(null);
+
+    expect(res).toEqual({
+      errors: true,
+      messagesTree: ["Failed"],
+    });
+  });
+
+  it("Should still consider a value as invalid if the first condition passes but the last one fails", () => {
+    const schema = object({})
+      .test(() => null)
+      .test(() => "Failed");
+
+    const res = schema.validate(null);
+
+    expect(res).toEqual({
+      errors: true,
+      messagesTree: ["Failed"],
     });
   });
 });
