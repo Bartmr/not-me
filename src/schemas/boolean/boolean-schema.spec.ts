53dc4c6ae6b40835b4a83eb7615c783cc2bd22a4
Bartolomeu <pbartmr@gmail.com>
2022-07-22 21:37:31 +0000

    Add more tests to boolean schema

--- src/schemas/boolean/boolean-schema.spec.ts
+++ src/schemas/boolean/boolean-schema.spec.ts
@@ -19,4 +19,73 @@ describe("Boolean Schema", () => {
       value: undefined,
     });
   });
+
+  it("Should transform boolean strings to booleans", () => {
+    const schema: Schema<boolean | undefined | null> = boolean();
+
+    expect(schema.validate("true")).toEqual({
+      errors: false,
+      value: true,
+    });
+
+    expect(schema.validate("false")).toEqual({
+      errors: false,
+      value: false,
+    });
+  });
+
+  it("Should be valid", () => {
+    const schema: Schema<boolean | undefined | null> = boolean();
+
+    expect(schema.validate(true)).toEqual({
+      errors: false,
+      value: true,
+    });
+
+    expect(schema.validate(false)).toEqual({
+      errors: false,
+      value: false,
+    });
+  });
+
+  it("Should be valid", () => {
+    const schema: Schema<boolean | undefined | null> = boolean();
+
+    expect(schema.validate("  true")).toEqual({
+      errors: false,
+      value: true,
+    });
+
+    expect(schema.validate("false  ")).toEqual({
+      errors: false,
+      value: false,
+    });
+  });
+
+  it("Should be invalid", () => {
+    const schema: Schema<boolean | undefined | null> = boolean();
+
+    expect(schema.validate("Hello")).toEqual({
+      errors: true,
+      messagesTree: ["Must be a boolean"],
+    });
+  });
+
+  it("Should be invalid", () => {
+    const schema: Schema<boolean | undefined | null> = boolean();
+
+    expect(schema.validate(1)).toEqual({
+      errors: true,
+      messagesTree: ["Must be a boolean"],
+    });
+  });
+
+  it("Should be invalid", () => {
+    const schema: Schema<boolean | undefined | null> = boolean();
+
+    expect(schema.validate(0)).toEqual({
+      errors: true,
+      messagesTree: ["Must be a boolean"],
+    });
+  });
 });
